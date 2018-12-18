9ef7f89c68f5dd7c5f3c40af294dbeab6881c622
luunamten <31501216+luunamten@users.noreply.github.com>
2018-12-18 13:19:31 +0700

    change

--- app/src/main/java/org/nam/fragment/MyMapFragment.java
+++ app/src/main/java/org/nam/fragment/MyMapFragment.java
@@ -1,8 +1,11 @@
 package org.nam.fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,12 +19,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import org.nam.R;
+import org.nam.firebase.IResult;
+import org.nam.util.LocationUtils;
 
 public class MyMapFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap map;
+    private Context context;
     public MyMapFragment() {
         // Required empty public constructor
     }
@@ -37,13 +47,49 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
 
+    @SuppressLint("MissingPermission")
     @Override
-    public void onMapReady(GoogleMap map) {
+    public void onMapReady(final GoogleMap map) {
         this.map = map;
+        map.setMyLocationEnabled(true);
+        getLastLocation(new IResult<Location>() {
+            @Override
+            public void onResult(Location result) {
+                LatLng location = new LatLng(result.getLatitude(), result.getLongitude());
+                map.moveCamera(CameraUpdateFactory.newLatLng(location));
+            }
+            @Override
+            public void onFailure(@NonNull Exception exp) {
+            }
+        });
+    }
+
+    private void getLastLocation(final IResult<Location> result) {
+        LocationUtils.getLastLocation(context)
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if(location == null) {
+                            return;
+                        }
+                        result.onResult(location);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        result.onFailure(e);
+                    }
+        });
     }
 
 }
