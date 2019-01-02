f9bc4de3a23b80120ec6f762f43179aa41cb97ff
minhtran0918 <minhtran0918@gmail.com>
2019-01-02 14:26:09 +0700

    2-1

--- app/src/main/java/org/nam/listener/DistrictSpinnerItemSelectedListener.java
+++ app/src/main/java/org/nam/listener/DistrictSpinnerItemSelectedListener.java
@@ -22,17 +22,18 @@ import java.util.List;
 
 public class DistrictSpinnerItemSelectedListener implements AdapterView.OnItemSelectedListener {
     private IUseAddressSpinner user;
+
     public DistrictSpinnerItemSelectedListener(IUseAddressSpinner user) {
         this.user = user;
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        if(view == null) {
+        if (view == null) {
             return;
         }
         final Spinner townSpinner = user.getTownSpinner();
-        if(townSpinner == null) {
+        if (townSpinner == null) {
             return;
         }
         final Context context = view.getContext();
@@ -40,25 +41,19 @@ public class DistrictSpinnerItemSelectedListener implements AdapterView.OnItemSe
         final List<Town> towns = new ArrayList<>();
         towns.add(new Town(-1,
                 context.getString(R.string.townLabel)));
-        if(selectedDistrict.getId() != -1) {
+        if (selectedDistrict.getId() != -1) {
             final AddressDBConnector connector = AddressDBConnector.getInstance();
             towns.addAll(connector.getTownsFromDistrict(
                     selectedDistrict.getId()));
         }
         Log.w("ffffff", String.valueOf(view.getContext() == MyApp.getContext()));
-        final ArrayAdapter<Town> adapter = new ArrayAdapter<>(
-<<<<<<< HEAD
-                context, android.R.layout.simple_spinner_item, towns);
-=======
-                MyApp.getContext(),
-                android.R.layout.simple_spinner_item, towns);
->>>>>>> fc8f545d792cb6bdd19573534bf933afe1ebc54b
+        final ArrayAdapter<Town> adapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, towns);
         adapter.setDropDownViewResource(
                 android.R.layout.simple_spinner_dropdown_item);
         townSpinner.setAdapter(adapter);
         final SharedPreferences dataStore = MyApp.getContext().getSharedPreferences(Contract.SHARED_MY_STATE,
                 Context.MODE_PRIVATE);
-        if(position != dataStore.getInt(Contract.SHARED_DISTRICT_KEY, 0)) {
+        if (position != dataStore.getInt(Contract.SHARED_DISTRICT_KEY, 0)) {
             final SharedPreferences.Editor editor = dataStore.edit();
             editor.putInt(Contract.SHARED_DISTRICT_KEY, position);
             editor.putInt(Contract.SHARED_TOWN_KEY, 0);
